3ec26fddb4280e47f40d5d751c0ef6075383d5eb
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2022-06-27 18:21:12 +0530

    build: MONGODB_URI hidden

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,15 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+function generateTokenForUser(user, role) {
+    const token = jwt.sign({ user: user, role: role }, process.env.TOKEN_SECRET);
+    return token;
+}
 
 const generateToken = (req, res) => {
     const userid = req.query.userid;
     const role = req.query.role;
-    const token = generateToken(userid, role)
+    const token = generateTokenForUser(userid, role)
     res.send(token);
     console.log(token);
 }
